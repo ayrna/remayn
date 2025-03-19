3108bbeb9836ff99101c56433ebbf0fb812d995a
Victor Vargas <vvargas@uco.es>
2025-03-19 11:47:22 +0100

    revert conflicts fix

--- remayn/report/__init__.py
+++ remayn/report/__init__.py
@@ -1,8 +1,6 @@
-from .dataframe import create_2d_report
 from .excel import create_excel_columns_report, create_excel_summary_report
 
 __all__ = [
     "create_excel_summary_report",
     "create_excel_columns_report",
-    "create_2d_report",
 ]
