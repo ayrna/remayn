99c84037eefd05821a50c5312eb48bb7d431f133
Victor Vargas <vvargas@uco.es>
2025-03-19 11:28:57 +0100

    Revert "create_latex_2d_2values_report"
    This reverts commit 539c2b4fb93910cca5fccb600f61aae765329afc.

--- remayn/report/__init__.py
+++ remayn/report/__init__.py
@@ -1,10 +1,8 @@
 from .dataframe import create_2d_report
 from .excel import create_excel_columns_report, create_excel_summary_report
-from .latex import create_latex_2d_2values_report
 
 __all__ = [
     "create_excel_summary_report",
     "create_excel_columns_report",
     "create_2d_report",
-    "create_latex_2d_2values_report",
 ]
